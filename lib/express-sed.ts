12e5f4a3f1f06516989d8a894a4f3d8b7ee8ceeb
kawanet <u-suke@kawa.net>
2020-05-06 16:19:37 +0900

    Content-Encoding: gzip

--- lib/express-sed.ts
+++ lib/express-sed.ts
@@ -1,10 +1,14 @@
 // express-sed.ts
 
 import * as express from "express";
+import {gunzip, inflate} from "zlib";
+import {promisify} from "util";
 
 type replaceFn = (str: string) => string;
-type chunkItem = [string | Buffer, any, any];
+type chunkItem = [string | Buffer, any?, any?];
 type numMap = { [type: string]: number };
+type decoderFn = (buffer: Buffer) => Promise<Buffer>;
+type callbackFn = (err?: Error) => void;
 
 const textType: numMap = {
     "javascript": 1,
@@ -22,6 +26,11 @@ const removeHeaders: numMap = {
     "if-unmodified-since": 1,
     "range": 1,
 };
+
+const decoders = {
+    gzip: promisify(gunzip),
+    deflate: promisify(inflate),
+} as { [encoding: string]: decoderFn };
 
 export function sed(transform: (string | replaceFn)): express.RequestHandler {
     if ("string" === typeof transform) {
@@ -41,55 +50,105 @@ export function sed(transform: (string | replaceFn)): express.RequestHandler {
         // remove conditional request headers
         Object.keys(removeHeaders).forEach(key => delete req.headers[key]);
 
-        res.write = function (chunk: any, encoding?: any, cb?: any) {
+        res.write = function (chunk: any, encoding?: any, cb?: callbackFn) {
             if (ended) return false;
-
-            const args = [].slice.call(arguments);
-            if ("function" === typeof args[args.length - 1]) cb = args.pop();
-            queue.push(args);
+            cb = enqueue([].slice.call(arguments));
             if (cb) cb();
-
             return true;
         };
 
-        res.end = function (chunk?: any, encoding?: any, cb?: any) {
+        res.end = function (chunk?: any, encoding?: any, cb?: callbackFn) {
             if (ended) return false;
             ended = true;
+            cb = enqueue([].slice.call(arguments));
 
-            // chunk argument could be omitted
-            let args = [].slice.call(arguments);
-            if ("function" === typeof args[args.length - 1]) cb = args.pop();
-            if (args[0]) queue.push(args);
-            args = cb ? [cb] : [];
-
-            const type = this.get("Content-Type");
-            if (isText(type)) {
-                // Concatenate
-                let text = queue.map(item => item[0]).filter(chunk => chunk).join("");
-
-                // Replace response body
-                text = transform(text) || "";
-                const data = Buffer.from(text);
-                if (!isHEAD) args.unshift(data);
-
-                // Content-Length:
-                this.set("Content-Length", (+data.length) + "");
-
-                // ETag:
-                var etagFn = this.app && this.app.get('etag fn')
-                if ("function" === typeof etagFn) {
-                    this.set("ETag", etagFn(data));
-                }
-            } else if (!isHEAD) {
-                // ignore binary content
-                queue.forEach(item => _write.apply(this, item));
-            }
-
-            return _end.apply(this, args);
+            sendBody(this, queue).then(() => {
+                _end.call(this);
+            }, err => {
+                res.status(500);
+                _end.call(this);
+                return err;
+            }).then(cb);
         }
 
         next();
+
+        function enqueue(item: chunkItem): callbackFn {
+            let cb: callbackFn;
+            if ("function" === typeof item[item.length - 1]) cb = item.pop();
+            if (item[0]) queue.push(item);
+            return cb;
+        }
+
+        async function sendBody(res: express.Response, items: chunkItem[]) {
+            const type = res.get("Content-Type");
+
+            if (isText(type)) {
+                items = await applyTransform(res, items);
+            }
+
+            if (!isHEAD) items.forEach(item => _write.apply(res, item));
+        }
     };
+
+    async function applyTransform(res: express.Response, items: chunkItem[]): Promise<chunkItem[]> {
+        const buffers = items.map(item => item[0]).filter(chunk => chunk);
+
+        // concatenate
+        const source = await getBodyString(res, buffers);
+
+        // replace response body
+        const text = (transform as replaceFn)(source) || "";
+
+        // string to Buffer
+        const data = Buffer.from(text);
+
+        // Content-Length:
+        res.set("Content-Length", (+data.length) + "");
+
+        // ETag:
+        var etagFn = res.app && res.app.get('etag fn')
+        if ("function" === typeof etagFn) {
+            res.set("ETag", etagFn(data));
+        }
+
+        return [[data]];
+    }
+
+    async function getBodyString(res: express.Response, buffers: (string | Buffer)[]): Promise<string> {
+        const stringLength = buffers.filter(chunk => "string" === typeof chunk).length;
+
+        // shortcut when only string chunks given and no Buffer chunks mixed
+        if (stringLength === buffers.length) {
+            return buffers.join("");
+        }
+
+        // force Buffer
+        buffers = buffers.map(chunk => Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk));
+
+        // concat Buffer
+        let buffer = Buffer.concat(buffers as Buffer[]);
+
+        // uncompress Buffer
+        buffer = await uncompressBody(res, buffer);
+
+        // Buffer to string
+        return buffer.toString();
+    }
+
+    async function uncompressBody(res: express.Response, buffer: Buffer): Promise<Buffer> {
+        const contentEncoding = res.getHeader("content-encoding") as string;
+        const transferEncoding = res.getHeader("transfer-encoding") as string;
+        const decoder = decoders[contentEncoding] || decoders[transferEncoding];
+
+        if (decoder && buffer.length) {
+            buffer = await decoder(buffer);
+            res.removeHeader("content-encoding");
+            res.removeHeader("transfer-encoding");
+        }
+
+        return buffer;
+    }
 }
 
 /**
---
+++ test/60.gzip.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env mocha -R spec
+
+import * as express from "express";
+import * as request from "supertest";
+
+import {gzip, deflate} from "zlib";
+import {promisify} from "util";
+
+import {sed} from "../lib/express-sed";
+
+const TITLE = __filename.split("/").pop();
+
+const encoders = {
+    gzip: promisify(gzip),
+    deflate: promisify(deflate),
+} as any;
+
+describe(TITLE, () => {
+    const agent = getAgent();
+
+    test("/without-sed/gzip", "sample:gzip");
+    test("/without-sed/deflate", "sample:deflate");
+    test("/with-sed/gzip", "SAMPLE:gzip");
+    test("/with-sed/deflate", "SAMPLE:deflate");
+
+    function test(path: string, expected: string) {
+        it(path, () => {
+            return agent.get(path).expect(expected);
+        });
+    }
+});
+
+function getAgent() {
+    const app = express();
+    app.get("/without-sed/:encoding", sampleAPI());
+    app.use(sed("s/sample/SAMPLE/g"));
+    app.get("/with-sed/:encoding", sampleAPI());
+    return request(app);
+}
+
+function sampleAPI(): express.RequestHandler {
+    return async (req, res, next) => {
+        const encoding = req.params.encoding;
+        const body = "sample:" + encoding;
+        let buffer = Buffer.from(body);
+
+        const encoder = encoders[encoding];
+        if (encoder) buffer = await encoder(buffer);
+
+        res.status(200);
+        res.header("Content-Type", "text/plain");
+        res.header("Content-Encoding", encoding);
+        res.header("Content-Length", "" + buffer.length);
+
+        // chunked response body
+        [].map.call(buffer, (byte: number) => res.write(Buffer.from([byte])));
+
+        res.end();
+    };
+}
